1c87ea2d0b39bf0f24551f892e08370c24fe4363
Krishna Gupta <krishna.gupta@intelegencia.com>
2026-01-12 19:13:39 +0530

    Replace default classes with domain entities

--- SplitWise.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SplitWise.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ SplitWise.Domain/Entities/Group.cs
@@ -0,0 +1,16 @@
+﻿using SplitWise.Domain.Comman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitWise.Domain.Entities
+{
+    public class Group: BaseEntity
+    {
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+    }s
+}
