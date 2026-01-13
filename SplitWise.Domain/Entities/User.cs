2ee81b646ba7131be01abd2b9bff9fefca366456
Avinash Upadhyay <avinash.upadhyay@intelegencia.com>
2026-01-13 20:59:47 +0530

    modify identity user, create context class for db

--- SplitWise.Domain/Entities/User.cs
+++ SplitWise.Domain/Entities/User.cs
@@ -1,29 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
-using SplitWise.Domain.Comman;
+﻿//using System.ComponentModel.DataAnnotations;
+//using SplitWise.Domain.Comman;
 
-namespace SplitWise.Domain.Entities
-{
-    public class User: BaseEntity
-    {
-        [Required]
-        [StringLength(100)]
-        public string? AccountName { get; set; }
+//namespace SplitWise.Domain.Entities
+//{
+//    public class User: BaseEntity
+//    {
+//        [Required]
+//        [StringLength(100)]
+//        public string? AccountName { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        [RegularExpression( @"^[^@\s]+@[^@\s]+\.[^@\s]+$")]
-        public string? Email { get; set; }
+//        [Required]
+//        [StringLength(100)]
+//        [RegularExpression( @"^[^@\s]+@[^@\s]+\.[^@\s]+$")]
+//        public string? Email { get; set; }
 
-        [Required]
-        public string? Password { get; set; }
+//        [Required]
+//        public string? Password { get; set; }
 
-        [Required]
-        public string? FName { get; set; }
+//        [Required]
+//        public string? FName { get; set; }
 
-        [Required]
-        public string? LName { get; set; }
+//        [Required]
+//        public string? LName { get; set; }
 
-        [StringLength (15)]
-        public string? MobileNumber { get; set; }
-    }
-}
+//        [StringLength (15)]
+//        public string? MobileNumber { get; set; }
+//        public ICollection<GroupMember> GroupMembers { get; set; } = new HashSet<GroupMember>();
+//    }
+//}
--- SplitWise.Infrastructure/Data/ApplicationDbContext.cs
+++ SplitWise.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,13 +1,40 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SplitWise.Domain.Entities;
+using SplitWise.Infrastructure.Identity;
 
-namespace SplitWise.MVC.Data
+namespace SplitWise.Infrastructure.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext
+        : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<GroupMember> GroupMembers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            builder.Entity<GroupMember>()
+                .HasOne<ApplicationUser>()
+                .WithMany(u => u.GroupMembers)
+                .HasForeignKey(gm => gm.UserId)
+                .IsRequired();
+
+ 
+            builder.Entity<GroupMember>()
+                .HasOne(gm => gm.Group)
+                .WithMany(g => g.GroupMembers)
+                .HasForeignKey(gm => gm.GroupId)
+                .IsRequired();
+        }
     }
 }
+
